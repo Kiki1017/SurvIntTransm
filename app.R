f4262a2095cae21cc303750e77059b2873bcbfea
Renata Retkute <r.retkute@yahoo.com>
2020-02-19 21:45:16 +0000

    Add files via upload

--- app.R
+++ app.R
@@ -96,9 +96,87 @@ server = function(input, output) {
     
     if (is.null(inFile))  {
       x <- read.csv("earlyCases.csv")
+      df <- data.frame(id=1:nrow(x),days=x[,1])
+      df$upper <- ifelse(df$days==0,0,df$days+1)
+      df$lower <- ifelse(df$days==0,0,df$days-1)
+      
+      fig1<-ggplot(data = df, aes(x=days, color='red')) +
+        geom_histogram( binwidth  = 1, fill="white", show.legend = FALSE, size=1.1) +
+        ggtitle('Observed time from symptoms onset to hospitalisation')  + xlab('Days')+ ylab("Counts")
+      
+      days<-seq(0.1, 15, 0.1)
+      
+      if(input$distribution=='Exponential'){
+          fit1<-readRDS("default_fit_exp.rds")
+          res <- extract(fit1)
+          param<-res$lambda
+          
+          ind<-sample(1:length(param), 50, replace=F)
+          dots <- data.frame(x=days, y=dexp(days, param[ind[1]]))
+          
+          fig2 <-ggplot(data = dots, aes(x=x,y=y))+ geom_line(color= 'gray', size=0.5)  +
+            labs(x = 'Days', y='PDF') + ggtitle('Fitted time from symptoms onset to hospitalisation') 
+          
+          for(i in 2:length(ind)){
+            dots <- data.frame(x=days, y=dexp(days, param[ind[i]]))
+            fig2<- fig2 +  geom_line(data = dots, aes(x=x,y=y), color= 'gray', size=0.5)
+          }
+          
+          prob0<-p.exp(param, 0, m)
+          prob1<-p.exp(param, rho, m)
+          lm0<-as.numeric(quantile(prob0,c(0.05, 0.95)))
+          lm1<-as.numeric(quantile(prob1,c(0.05, 0.95)))
+          
+          ans<-data.frame(cases=c("No intensification", "With intensification"), prob=c(mean(prob0), mean(prob1)), low=c(lm0[1], lm1[1]), upp=c(lm0[2], lm1[2]))
+          ans$cases<-as.factor(ans$cases)
+          
+          log_lik1 <- extract_log_lik(fit1, merge_chains = FALSE)
+          rel_n_eff <- relative_eff(exp(log_lik1))
+          loo.sum<-loo(log_lik1, r_eff = rel_n_eff, cores = 2)
+          
+          fig3 <- ggplot(ans, aes(x=cases, y=prob)) +    geom_bar(color="black", fill="blue", stat = "identity")  + labs(x="Surveilance", y = "Prob(Sustained transmission)") + theme_bw() +ggtitle("Surveilance intensification")  +ylim(c(0,1)) # +  geom_errorbar(aes(ymin=low, ymax=upp), width=.2,  position=position_dodge(.9)) + theme(text = element_text(size=12))
+          fig4<-tableGrob(loo.sum$estimates)
+          
+      }  else {
+        fit1<-readRDS("default_fit_gam.rds")
+        res <- extract(fit1)
+        
+        param1<-res$alpha 
+        param2<-res$beta
+        
+        ind<-sample(1:length(param1), 50, replace=F)
+        dots <- data.frame(x=days, y=dgamma(days, shape=param1[ind[1]], rate=param2[ind[1]]))
+        
+        fig2 <-ggplot(data = dots, aes(x=x,y=y))+ geom_line(color= 'gray', size=0.5)  +
+          labs(x = 'Days', y='PDF') + ggtitle('Fitted infection period') 
+        
+        for(i in 2:length(ind)){
+          dots <- data.frame(x=days, y=dgamma(days, shape= param1[ind[i]], rate=param2[ind[i]]))
+          fig2<- fig2 +  geom_line(data = dots, aes(x=x,y=y), color= 'gray', size=0.5)
+        }
+        
+        
+        prob0<-sapply(1:length(param1), function(a) p.gam(param1[a], param2[a], 0, m))
+        prob1<-sapply(1:length(param1), function(a) p.gam(param1[a], param2[a], rho, m))
+        lm0<-as.numeric(quantile(prob0,c(0.05, 0.95)))
+        lm1<-as.numeric(quantile(prob1,c(0.05, 0.95)))
+        
+        ans<-data.frame(cases=c("No intensification", "With intensification"), prob=c(mean(prob0), mean(prob1)), low=c(lm0[1], lm1[1]), upp=c(lm0[2], lm1[2]))
+        ans$cases<-as.factor(ans$cases)
+        
+        log_lik1 <- extract_log_lik(fit1, merge_chains = FALSE)
+        rel_n_eff <- relative_eff(exp(log_lik1))
+        loo.sum<-loo(log_lik1, r_eff = rel_n_eff, cores = 2)
+        
+        
+        fig3 <- ggplot(ans, aes(x=cases, y=prob)) +    geom_bar(color="black", fill="blue", stat = "identity")  + labs(x="Surveilance", y = "Prob(Sustained transmission)") + theme_bw() +ggtitle("Surveilance intensification")  +ylim(c(0,1)) # +  geom_errorbar(aes(ymin=low, ymax=upp), width=.2,  position=position_dodge(.9)) + theme(text = element_text(size=12))
+        fig4<-tableGrob(loo.sum$estimates)
+      }
+      
+      
+      
      } else {
 		  x <- readdatafile()
-     }
     df <- data.frame(id=1:nrow(x),days=x[,1])
     df$upper <- ifelse(df$days==0,0,df$days+1)
     df$lower <- ifelse(df$days==0,0,df$days-1)
@@ -180,7 +258,8 @@ server = function(input, output) {
      
      fig3 <- ggplot(ans, aes(x=cases, y=prob)) +    geom_bar(color="black", fill="blue", stat = "identity")  + labs(x="Surveilance", y = "Prob(Sustained transmission)") + theme_bw() +ggtitle("Surveilance intensification")  +ylim(c(0,1)) # +  geom_errorbar(aes(ymin=low, ymax=upp), width=.2,  position=position_dodge(.9)) + theme(text = element_text(size=12))
      fig4<-tableGrob(loo.sum$estimates)
-   }
+     }
+     }
     grid.arrange(fig1, fig4, fig2 ,fig3, nrow=2, widths=c(2,1.5))
   })
    
